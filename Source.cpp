98fc4c0e088fc429f2c0c647790616689c6d2658
KaterunaR <131966803+KaterunaR@users.noreply.github.com>
2023-09-22 08:49:34 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
-#include <math.h>
+#include <cmath> 
 using namespace std;
 
-class Function {
+class FunctionB {
 public:
-	double y = 5.17;
-	double z = -9.52;
-	double b;
+    
+    double y = 5.17;
+    double z = -9.52;
+    double x = 5.28;
+    double b = ((1 + pow(cos(y - 2 * pow(x, 3)), 2)) / (2 + pow((abs(x)), 3 / 2) - pow(sin(pow((abs(z)), 1. / 2)), 1. / 2))) + pow((log(abs(z - y))), 2);
 
-	void Calculate() {
-		//b
-		for (double x = -1; x <= 1; x += 0.2) {
-			b = ((1 + pow(cos(y - 2 * pow(x, 3)), 2)) / (2 + pow((abs(x)), 3 / 2) - pow(sin(pow((abs(z)), 1. / 2)), 1. / 2))) + pow((log(abs(z - y))), 2);
-			cout << "b = " << b << endl;
-		}
-		//a
-		for (double x = -1; x <= 1; x += 0.2) {
-			double a = pow(y, 2) + (z + pow((sin(pow((abs(x + b)), 1. / 3))), 2)) / (pow(z, 2) + abs((pow(x, 2) + y) / (y + pow(x, 3) / 3)) - pow((log(abs(z))), 2));
-			cout << "a = " << a << endl;
-		}
-	}
 };
 
-int main() 
-{
-	Function a_b;
-	a_b.Calculate();
+class FunctionA {
+public:
+    double y = 5.17;
+    double z = -9.52;
+    double x = 5.28;
+    FunctionB b;
+    double new_b = b.b; 
 
-	return 0;
+    double a = pow(y, 2) + (z + pow((sin(pow((abs(x + b.b)), 1. / 3))), 2)) / (pow(z, 2) + abs((pow(x, 2) + y) / (y + pow(x, 3) / 3)) - pow((log(abs(z))), 2));
+};
+
+int main() {
+
+    FunctionB value_b;
+    cout << "b = " << value_b.b << endl;
+
+    FunctionA value_a;
+    cout << "a = " << value_a.a;
+
+    return 0;
 }
+
